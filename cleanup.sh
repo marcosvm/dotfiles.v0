2483ca5e2c53a6816a4d89b1ab63c1877bad0abb
Marcos Oliveira <marcosvm@gmail.com>
2019-03-24 16:49:02 -0700

    add alacritty configuration

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 
 # Nvim
 [ -f ~/.config/nvim/init.vim ] && rm ~/.config/nvim/init.vim
+
+# Alacritty
+[ -f ~/.config/alacritty/alacritty.yml ] && rm ~/.config/alacritty/alacritty.yml
--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ set -euxo pipefail
 # Nvim
 [ -d ~/.config/nvim ] || mkdir -p ~/.config/nvim
 [ -f ~/.config/nvim/init.vim ] || ln -s "${PWD}/init.vim" ~/.config/nvim/init.vim
+
+# Alacritty
+[ -d ~/.config/alacritty ] || mkdir -p ~/.config/alacritty
+[ -f ~/.config/alacritty/alacritty.yml ] || ln -s "${PWD}/alacritty.yml" ~/.config/alacritty/alacritty.yml
